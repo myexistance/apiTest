eb9e580eb7a838ed5739eedde05799adfb78f457
Joon Yoon <myexistance@HOTMAIL.COM>
2022-11-03 17:20:31 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+// import axios
+import Axios from "axios";
+import { useState } from 'react';
+
+// jest.mock("axios");
+
 
 function App() {
+const [name,setName] = useState("");
+// const [canrelist, setCanrelist] = useState("");
+// const [description, setDescription] = useState("");
+
+
+//fetch API using axios
+Axios.get("https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false").then((res) => {
+  setName(res.data.Name);
+  console.log ()
+  // setCanrelist(res.data.CanRelist);
+  // setCanrelist(res.data.Description);
+});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Name: {name}</p> 
+        {/* <p>CanRelist: {canrelist}</p>   */}
+        {/* <p>Description: {description}</p> */}
     </div>
   );
 }
