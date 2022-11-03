f2e2c5c7ba637a14dc18cbc7c9c20fb22c0ecdb0
Joon Yoon <myexistance@HOTMAIL.COM>
2022-11-03 17:58:37 +1300

    .

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useState } from 'react';
 function App() {
 const [name,setName] = useState("");
 // const [canrelist, setCanrelist] = useState("");
-// const [description, setDescription] = useState("");
+const [description, setDescription] = useState("");
 
 
 //fetch API using axios
@@ -17,13 +17,13 @@ Axios.get("https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue
   setName(res.data.Name);
   console.log ()
   // setCanrelist(res.data.CanRelist);
-  // setCanrelist(res.data.Description);
+  setDescription(res.data[4].Description);
 });
   return (
     <div className="App">
         <p>Name: {name}</p> 
         {/* <p>CanRelist: {canrelist}</p>   */}
-        {/* <p>Description: {description}</p> */}
+        <p>Description: {description}</p>
     </div>
   );
 }
