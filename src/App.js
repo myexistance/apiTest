d506caaadeaa22fc6f4330befba4429001f0f55f
Joon Yoon <myexistance@HOTMAIL.COM>
2022-11-03 19:25:52 +1300

    working on App.test

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import './App.css';
 import Axios from "axios";
 import { useState } from 'react';
 
-// jest.mock("axios");
-
 
 function App() {
+
 const [name,setName] = useState("");
 // const [canrelist, setCanrelist] = useState("");
-const [description, setDescription] = useState("");
+// const [description, setDescription] = useState("");
+
+
+
 
 
 //fetch API using axios
 Axios.get("https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false").then((res) => {
   setName(res.data.Name);
-  console.log ()
-  // setCanrelist(res.data.CanRelist);
-  setDescription(res.data[4].Description);
 });
   return (
     <div className="App">
+        
         <p>Name: {name}</p> 
-        {/* <p>CanRelist: {canrelist}</p>   */}
-        <p>Description: {description}</p>
+  
     </div>
   );
 }
+
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Axios from "axios";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+Axios.get("https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false").then((res) => {
+  setName(res.data.Name);
+});
+
+test('name equals badges', () => {
+  expect(res.data.Name).toEqual("Badges");
 });
